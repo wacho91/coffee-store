d7818030e8a517a44b4b3a42520efddff74e4314
wacho91 <criskol.71@gmail.com>
2024-07-20 18:29:36 -0500

    Footer terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AppBanner from "./components/AppBanner/AppBanner"
+import Footer from "./components/Footer/Footer"
 import Hero from "./components/Hero/Hero"
 import Services from "./components/Services/Services"
 import WhereToBuy from "./components/WhereToBuy/WhereToBuy"
@@ -11,6 +12,7 @@ function App() {
       <Services />
       <WhereToBuy />
       <AppBanner />
+      <Footer />
     </div>
   )
 }
